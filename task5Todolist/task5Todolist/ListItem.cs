79c586c26971d6f3b19a8783ad723ae620c4196c
lexinteam <lbrk2019ssdd@gmail.com>
2021-06-30 11:02:27 -0700

    task5comm1

---
+++ task5Todolist/task5Todolist/ListItem.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task5Todolist
+{
+    class ListItem
+    {
+        public string Name { get; set; }
+        public int Difficulty { get; set; }
+
+        Priority pName;
+
+        public ListItem()
+        {
+            Console.WriteLine("Please enter task name");
+            Name = Console.ReadLine();
+            Console.WriteLine("Please enter Priority (High, Medium, Low)");
+            Enum.TryParse(Console.ReadLine(), out Priority pName);
+            Console.WriteLine("Please enter Difficulty (Difficult, Medium, Easy)");
+            string DiffString = Console.ReadLine();
+            if (DiffString == "Difficult")
+            {
+                Difficulty = 4;
+            }
+            else if (DiffString == "Medium")
+            {
+                Difficulty = 2;
+            }
+            else if (DiffString == "Easy")
+            {
+                Difficulty = 1;
+            }
+        }
+
+
+    }
+
+    enum Priority
+    {
+        High,
+        Medium,
+        Low
+    }
+}
---
+++ task5Todolist/task5Todolist/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace task5Todolist
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            List<ListItem> listItems = new List<ListItem>();
+
+
+
+            do
+            {
+
+                listItems.Add(new ListItem());
+
+                Console.WriteLine("\nWould you like to add another task? Y/N");
+
+                if (Console.ReadKey().Key == ConsoleKey.Y)
+                {
+                    continue;
+                }
+                else
+                {
+                    break;
+                }
+            }
+            while (true);
+
+            int timeAll = 0;
+
+
+            foreach (var i in listItems)
+            {
+                timeAll += i.Difficulty;
+            }
+
+            // to solve question marks instead of russian chars
+            Console.WriteLine("сколько всего времени нужно для выполнения всех задач" + timeAll + "часов");
+
+            // TODO 
+            //-список задач заданного приоритета(приоритет ввести с клавиатуры)
+            //- какие задачи возможно сделать за N дней с учетом приоритета(N ввести с клавиатуры)
+
+            //Console.WriteLine("\n" + listItems.Count);
+
+
+        }
+    }
+}
